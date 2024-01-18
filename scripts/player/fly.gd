a23558a4c9ee601ce67f672fa5aba6023b52dc75
Ignacio Rojas <ignaciorojasdev@hotmail.com>
2024-01-18 18:57:03 -0300

    added player as a fly

--- scripts/player/fly.gd
+++ scripts/player/fly.gd
@@ -1,9 +1,29 @@
 class_name Fly extends CharacterBody2D
 
 
+signal meter_traveled(amount_meters : int)
+
+
+@export var total_meters_traveled : int = 0
+
+
+# used for measuring the distance traveled in meters
+const METER_PER_PIXELS : int = 300
+# movement speed 
 const INITIAL_MOVEMENT_SPEED : float = 200
 # in degress
-const TURN_SPEED : float = 80
+const TURN_SPEED : float = 90
+
+
+var initial_position : Vector2
+
+
+func _ready():
+	initial_position = self.global_position
+
+
+func _process(delta):
+	_monitor_travel_info()
 
 
 func _physics_process(delta) -> void:
@@ -19,10 +39,20 @@ func _handle_turning(dt : float) -> void:
 	if sin(rotation_result) < -0.35:
 		self.global_rotation = rotation_result
 
-func _handle_movement():
+
+func _handle_movement() -> void:
 	self.velocity.x = cos(self.rotation) * INITIAL_MOVEMENT_SPEED
 	self.velocity.y = sin(self.rotation) * INITIAL_MOVEMENT_SPEED
 	move_and_slide()
 
 
-
+# checks the distance traveled 
+func _monitor_travel_info():
+	var distance : float = initial_position.distance_to(self.global_position)
+	var meters_traveled : int = distance / METER_PER_PIXELS
+	
+	if meters_traveled > total_meters_traveled:
+		meter_traveled.emit(meters_traveled)
+	
+	total_meters_traveled = meters_traveled
+	
